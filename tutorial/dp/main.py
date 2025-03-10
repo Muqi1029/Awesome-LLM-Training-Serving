7eb7a263e1cddc298091cc6b47dee444708e4d97
Muqi Li <muqi1029@gmail.com>
2025-03-10 22:16:37 +0800

    Update dp

--- tutorial/dp/main.py
+++ tutorial/dp/main.py
@@ -1,6 +1,8 @@
 import time
 
+import hydra
 import torch
+from omegaconf import DictConfig
 from torch import nn
 from torch.nn import functional as F
 from tqdm import tqdm
@@ -14,32 +16,16 @@ def compute_time(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print(f"Time taken: {end - start} seconds")
+        print(f"Time taken: {(end - start):.4f} seconds")
         return result
 
     return wrapper
 
 
-def main():
-    ds = DummyDataset(num_examples=1000, weights=[1, 2, 3])
-    dl = get_dataloader(ds)
-
-    model = nn.Sequential(
-        nn.Linear(3, 1),
-    )
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
-
-    if torch.cuda.device_count() > 1:
-        print(f"use {torch.cuda.device_count()} GPUs!")
-        model = nn.DataParallel(model)
-
+@compute_time
+def train(model, epochs, optimizer, dl, device):
     model.train()
-
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-    for epoch in tqdm(range(10)):
+    for epoch in tqdm(range(epochs)):
         for inputs, targets in dl:
             optimizer.zero_grad()
             inputs = inputs.to(device)
@@ -51,7 +37,33 @@ def main():
 
         print(f"Epoch {epoch} loss: {loss.item()}")
 
-    print(model.module.state_dict())
+
+@hydra.main(version_base=None, config_path=".", config_name="train")
+def main(config: DictConfig):
+    ds = DummyDataset(
+        num_examples=config.dataset.num_examples, weights=config.dataset.weight
+    )
+    dl = get_dataloader(ds)
+
+    model = nn.Sequential(nn.Linear(len(config.dataset.weight), 1))
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    if torch.cuda.device_count() > 1:
+        print(f"use {torch.cuda.device_count()} GPUs!")
+        model = nn.DataParallel(model)
+    else:
+        print("use single GPU")
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=config.training.lr)
+
+    train(model, config.training.epochs, optimizer, dl, device)
+
+    if torch.cuda.device_count() > 1:
+        print(model.module.state_dict())
+    else:
+        print(model.state_dict())
 
 
 if __name__ == "__main__":
