b2992ec1feaa6c4bd907edb6b000bd52f3496464
Muqi Li <muqi1029@gmail.com>
2025-12-19 14:37:12 +0800

    update

--- tools/request/simple_request.py
+++ tools/request/simple_request.py
@@ -33,25 +33,78 @@ tool_select_name = {
 
 tools = [tool_select_name]
 
+ebnf_content = """
+root ::= city | description
+
+city ::= "London" | "Pairis" | "Berlin" | "Rome"
+description ::= city " is " status
+
+status ::= "the capital of " country
+country ::= "England" | "France" | "Germany" | "Italy"
+"""
+
+# ebnf = """
+# root ::= number " + " number " = " number
+#
+# number ::= integer | float
+#
+# integer ::= digit+
+# float ::= number "." number
+# digit ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+# """
+
 
 def http_request(args):
+    url = f"{args.base_url}/v1/chat/completions"
+    headers = {"Authorization": f"Bearer {args.api_key}"}
+
+    # construct payload
+    payload = {}
+    if not args.msg_path and not args.input_ids_path and not args.payload_path:
+        payload = {"messages": [{"role": "user", "content": args.user_content}]}
+        if args.ebnf:
+            payload["ebnf"] = ebnf_content
+        elif args.tools:
+            payload["tools"] = tools
+    else:
+        if args.msg_path:
+            with open(args.msg_path, mode="r", encoding="utf-8") as f:
+                payload["messages"] = json.load(f)
+        elif args.payload_path:
+            with open(args.payload_path, mode="r", encoding="utf-8") as f:
+                payload = json.load(f)
+        elif args.input_ids_path:
+            with open(args.input_ids_path, mode="r", encoding="utf-8") as f:
+                input_ids = json.load(f)
+            from transformers import AutoTokenier
+
+            assert (
+                args.tokenizer_path
+            ), f"tokenizer_path must be provided in the input-ids-path"
+            tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
+            prompt = tokenizer.decode(input_ids)
+            print(f"{prompt=}")
+            payload["prompt"] = prompt
+            url = f"{args.base_url}/v1/completions"
+
     if args.disable_stream:
         res = requests.post(
-            url=f"{args.base_url}/v1/chat/completions",
-            headers={"Authorization": f"Bearer {args.api_key}"},
-            json={
-                "messages": [{"role": "user", "content": "Who are you?"}],
-            },
+            url,
+            headers=headers,
+            json=payload,
         )
-        res.raise_for_status()
-        print(res.json())
+        try:
+            res.raise_for_status()
+            print(res.json())
+        except Exception:
+            print(
+                f"\033[41m Request Error, Status Code={res.status_code}, Reason: {res.text} \033[0m"
+            )
     else:
         res = requests.post(
-            url=f"{args.base_url}/v1/chat/completions",
-            headers={"Authorization": f"Bearer {args.api_key}"},
-            json={
-                "messages": [{"role": "user", "content": "Who are you?"}],
-            },
+            url=url,
+            headers=headers,
+            json=payload,
             stream=True,
         )
         for line in res.iter_lines():
@@ -92,6 +145,11 @@ def openai_request(args):
         api_key=args.api_key,
     )
     model_id = list(client.models.list())[0].id
+
+    extra_body = {}
+    if args.enbf:
+        extra_body["ebnf"] = ebnf_content
+
     if args.disable_stream:
         response = client.chat.completions.create(
             model=model_id,
@@ -107,6 +165,7 @@ def openai_request(args):
                 }
             ],
             model=model_id,
+            extra_body=extra_body,
             # extra_body={
             #     "chat_template_kwargs": {"thinking": True}
             # },  # True or False to control model thinking
@@ -135,6 +194,18 @@ if __name__ == "__main__":
     parser.add_argument(
         "--backend", type=str, default="http", choices=["http", "openai"]
     )
+    parser.add_argument("--user-content", type=str, default="Who are you")
+    parser.add_argument("--tokenizer-path", type=str, help="The path of tokenizer path")
+
+    mutex_group = parser.add_mutually_exclusive_group()
+    mutex_group.add_argument(
+        "--ebnf", type="store_true", help="Constrained Decoding for EBNF format"
+    )
+    mutex_group.add_argument("--tool", action="store_true", help="Add tool")
+    mutex_group.add_argument("--msg-path", type=str, help="The path of messages")
+    mutex_group.add_argument("--payload-path", type=str, help="The path of payload")
+    mutex_group.add_argument("--input-ids-path", type=str, help="The path of input_ids")
+
     args = parser.parse_args()
     if args.backend == "http":
         http_request()
