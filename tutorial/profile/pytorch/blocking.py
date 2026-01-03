be30c2d72ffdb6b77a19ee0b2f87d406a65c33ae
Muqi Li <muqi1029@gmail.com>
2026-01-03 21:33:03 +0800

    Update

--- tutorial/profile/pytorch/blocking.py
+++ tutorial/profile/pytorch/blocking.py
@@ -5,7 +5,7 @@ import torch
 a = torch.randn((200, 100), pin_memory=True)
 
 tic_1 = perf_counter()
-a.cuda()
+a.cuda()  # blocking by default
 print(f"Blocking time: {(perf_counter() - tic_1):.6f}")
 
 
