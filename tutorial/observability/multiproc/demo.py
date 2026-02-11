c9a789fec340f386243847b1bf8fcafb88d30ae1
Muqi Li <muqi1029@gmail.com>
2026-02-11 11:47:51 +0800

    Add observability

---
+++ tutorial/observability/multiproc/demo.py
@@ -0,0 +1,66 @@
+import logging
+import os
+import re
+import tempfile
+
+import fastapi
+import psutil
+import uvicorn
+from starlette.routing import Mount
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def lifespan(app):
+    print("APP START UP")
+    yield
+    print("APP SHUTDOWN")
+
+
+app = fastapi.FastAPI(lifespan=lifespan)
+
+requests_counter = None
+cpu_usaga_gauge = None
+
+
+def create_metrics_recorders():
+    logger.info("Create metrics recorders")
+    global requests_counter, cpu_usaga_gauge
+    from prometheus_client import Counter, Gauge
+
+    requests_counter = Counter(
+        "demo_requests_total", "Total demo requests", labelnames=["model"]
+    )
+    cpu_usaga_gauge = Gauge("cpu_usage", "CPU usage", multiprocess_mode="livesum")
+
+
+@app.get("/")
+async def entrypoint():
+    assert requests_counter is not None and cpu_usaga_gauge is not None
+    requests_counter.labels(model="demo_model").inc()
+    cpu_percent = psutil.cpu_percent(interval=None)
+    cpu_usaga_gauge.set(cpu_percent)
+    return {"msg": "Hello, World", "cpu_percent": cpu_percent}
+
+
+prometheus_multiproc_dir = tempfile.TemporaryDirectory()
+os.environ["PROMETHEUS_MULTIPROC_DIR"] = prometheus_multiproc_dir.name
+logger.info(f"PROMETHEUS_MULTIPROC_DIR: {os.environ['PROMETHEUS_MULTIPROC_DIR']}")
+
+
+def mount_metrics():
+    logger.info("Start to mount metrics")
+    from prometheus_client import CollectorRegistry, make_asgi_app, multiprocess
+
+    registry = CollectorRegistry()
+    multiprocess.MultiProcessCollector(registry)
+    metrics_route = Mount("/metrics", make_asgi_app(registry))
+    metrics_route.path_regex = re.compile("^/metrics(?P<path>.*)$")
+    app.routes.append(metrics_route)
+    create_metrics_recorders()
+
+
+if __name__ == "__main__":
+    mount_metrics()
+    uvicorn.run(app)
